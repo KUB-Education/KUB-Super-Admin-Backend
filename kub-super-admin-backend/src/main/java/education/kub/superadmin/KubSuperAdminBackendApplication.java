f3cacc55caeeb7236510fa9fb157b46493f01945
Pavel Kilko <pavel.kilko@gmail.com>
2025-02-20 02:54:44 +0200

    feat: setup project

---
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/KubSuperAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package education.kub.superadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubSuperAdminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubSuperAdminBackendApplication.class, args);
+    }
+
+}
---
+++ kub-super-admin-backend/src/test/java/education/kub/superadmin/KubSuperAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package education.kub.superadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubSuperAdminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
