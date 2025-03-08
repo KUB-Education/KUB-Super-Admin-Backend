91fb8869b9474b07b6a16f80132d3811b78db92c
Pavel Kilko <pavel.kilko@gmail.com>
2025-03-09 00:14:24 +0200

    chore: remove redundant code for SmtpService

--- kub-super-admin-backend/src/main/java/education/kub/superadmin/services/inter/SmtpService.java
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/services/inter/SmtpService.java
@@ -3,7 +3,7 @@ package education.kub.superadmin.services.inter;
 import org.springframework.stereotype.Service;
 
 public interface SmtpService {
-    public boolean sendEmail(String to, String subject, String body);
+    boolean sendEmail(String to, String subject, String body);
 
-    public boolean checkConnection();
+    boolean checkConnection();
 }
