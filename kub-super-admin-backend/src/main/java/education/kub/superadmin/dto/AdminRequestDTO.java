64d3c48d8d74db0daf8844834a7d8a40e572544b
Pavel Kilko <pavel.kilko@gmail.com>
2025-03-10 13:46:23 +0200

    feat: add Jackson Annotations for DTOs

--- kub-super-admin-backend/src/main/java/education/kub/superadmin/dto/AdminRequestDTO.java
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/dto/AdminRequestDTO.java
@@ -1,30 +1,36 @@
 package education.kub.superadmin.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class AdminRequestDTO {
+    @JsonProperty("last_name")
     @NotBlank(message = "lastName can't be blank")
     @Size(min = 1, max = 32, message = "lastName must have length in interval [1,32]")
     @Pattern(regexp = "^[a-zA-Zа-яА-Я]+$", message = "lastName must contain only Latin or Cyrillic letters")
     private String lastName;
 
+    @JsonProperty("first_name")
     @NotBlank(message = "firstName can't be blank")
     @Size(min = 1, max = 32, message = "firstName must have length in interval [1,32]")
     @Pattern(regexp = "^[a-zA-Zа-яА-Я]+$", message = "firstName must contain only Latin or Cyrillic letters")
     private String firstName;
 
+    @JsonProperty("middle_name")
     @Size(min = 1, max = 32, message = "middleName must have length in interval [1,32]")
     @Pattern(regexp = "^[a-zA-Zа-яА-Я]+$", message = "middleName must contain only Latin or Cyrillic letters")
     private String middleName;
 
+    @JsonProperty("email")
     @NotBlank(message = "email can't be blank")
     @Size(max = 64, message = "email must have length <= 64")
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
--- kub-super-admin-backend/src/main/java/education/kub/superadmin/dto/AdminResponseDTO.java
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/dto/AdminResponseDTO.java
@@ -1,5 +1,6 @@
 package education.kub.superadmin.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import education.kub.superadmin.entities.UserEntity;
 import lombok.Builder;
 import lombok.Data;
@@ -7,11 +8,24 @@ import lombok.Data;
 @Builder
 @Data
 public class AdminResponseDTO {
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("user_id")
     private Long userId;
+
+    @JsonProperty("last_name")
     private String lastName;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("middle_name")
     private String middleName;
+
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("status")
     private UserEntity.Status status;
 }
