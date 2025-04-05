f32e5e90c0254d021d4a112bff8d23d66a946e6f
YaroslavDrovolskyi <78113965+YaroslavDrovolskyi@users.noreply.github.com>
2025-04-05 22:52:19 +0300

    fix: rename 'field of study' table in migrations

--- kub-super-admin-backend/src/main/resources/db/migration/V1_5__create_study_field_table.sql
+++ kub-super-admin-backend/src/main/resources/db/migration/V1_5__create_study_field_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS public.field_of_study
+CREATE TABLE IF NOT EXISTS public.study_field
 (
     id bigserial, -- implicitly adds NOT NULL, creates sequence, assigns DEFAULT according to sequence
     code character varying(32) COLLATE pg_catalog."default" NOT NULL,
     name character varying(128) COLLATE pg_catalog."default" NOT NULL,
 
-    CONSTRAINT field_of_study_pkey PRIMARY KEY (id),
-    CONSTRAINT field_of_study_code_unique UNIQUE (code),
+    CONSTRAINT study_field_pkey PRIMARY KEY (id),
+    CONSTRAINT study_field_code_unique UNIQUE (code),
     CONSTRAINT code_check CHECK (code::text <> ''::text) NOT VALID, -- not empty
     CONSTRAINT name_check CHECK (name::text <> ''::text) NOT VALID -- not empty
     )
--- kub-super-admin-backend/src/main/resources/db/migration/V1_6__create_specialty_table.sql
+++ kub-super-admin-backend/src/main/resources/db/migration/V1_6__create_specialty_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.specialty
 (
     id bigserial, -- implicitly adds NOT NULL, creates sequence, assigns DEFAULT according to sequence
-    field_of_study_id bigserial NOT NULL,
+    study_field_id bigserial NOT NULL,
     code character varying(32) COLLATE pg_catalog."default" NOT NULL,
     name character varying(128) COLLATE pg_catalog."default" NOT NULL,
 
@@ -9,10 +9,10 @@ CREATE TABLE IF NOT EXISTS public.specialty
     CONSTRAINT specialty_code_unique UNIQUE (code),
     CONSTRAINT code_check CHECK (code::text <> ''::text) NOT VALID, -- not empty
     CONSTRAINT name_check CHECK (name::text <> ''::text) NOT VALID, -- not empty
-    CONSTRAINT field_of_study_id_fkey FOREIGN KEY (field_of_study_id) -- foreign key
-    REFERENCES public.field_of_study (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    )
+    CONSTRAINT study_field_id_fkey FOREIGN KEY (study_field_id) -- foreign key
+        REFERENCES public.study_field (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
 TABLESPACE pg_default;
