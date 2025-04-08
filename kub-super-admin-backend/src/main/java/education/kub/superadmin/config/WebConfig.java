1e98d7e417799c1998076c256798af4a420dcb20
Pavel Kilko <pavel.kilko@gmail.com>
2025-04-09 01:23:18 +0300

    refactor: remove redundant @Autowired for WebConfig

--- kub-super-admin-backend/src/main/java/education/kub/superadmin/config/WebConfig.java
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/config/WebConfig.java
@@ -1,7 +1,6 @@
 package education.kub.superadmin.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
     private final AdminHeaderHandler adminHeaderInterceptor;
 
     @Override
