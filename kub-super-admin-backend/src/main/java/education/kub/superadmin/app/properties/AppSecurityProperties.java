360b2d48e9d254ed3eb3bd489900c74ea6e7080a
Pavel Kilko <pavel.kilko@gmail.com>
2025-04-12 03:01:49 +0300

    feat: add app properties

--- kub-super-admin-backend/src/main/java/education/kub/superadmin/app/properties/AppSecurityProperties.java
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/app/properties/AppSecurityProperties.java
@@ -1,8 +1,8 @@
-package education.kub.superadmin.config;
+package education.kub.superadmin.app.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.security")
 public record AppSecurityProperties(
-        int temporaryPasswordExpirationDays
+        String superAdminHeaderSecret
 ) {}
