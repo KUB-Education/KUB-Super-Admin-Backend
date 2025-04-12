6f9dccc0cb5a2416de9c65dd41f21e688080f520
Pavel Kilko <pavel.kilko@gmail.com>
2025-04-12 03:01:30 +0300

    feat: update super admin header interceptor

--- kub-super-admin-backend/src/main/java/education/kub/superadmin/app/interceptor/SuperAdminHeaderInterceptor.java
+++ kub-super-admin-backend/src/main/java/education/kub/superadmin/app/interceptor/SuperAdminHeaderInterceptor.java
@@ -1,26 +1,30 @@
-package education.kub.superadmin.config;
+package education.kub.superadmin.app.interceptor;
 
+import education.kub.superadmin.app.properties.AppSecurityProperties;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
-public class AdminHeaderHandler implements HandlerInterceptor {
+@RequiredArgsConstructor
+public class SuperAdminHeaderInterceptor implements HandlerInterceptor {
 
-    @Value("${super.admin.key}")
-    private String superAdminSecret;
     private static final String ADMIN_KEY_HEADER = "X-SUPER-ADMIN-KEY";
+
+    private final AppSecurityProperties appSecurityProperties;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String headerValue = request.getHeader(ADMIN_KEY_HEADER);
 
-        if (headerValue == null || !headerValue.equals(superAdminSecret)) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Missing or invalid X-SUPER-ADMIN-KEY");
+        if (headerValue == null || !headerValue.equals(appSecurityProperties.superAdminHeaderSecret())) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Missing or invalid " + ADMIN_KEY_HEADER);
+
             return false;
         }
+
         return true;
     }
 }
