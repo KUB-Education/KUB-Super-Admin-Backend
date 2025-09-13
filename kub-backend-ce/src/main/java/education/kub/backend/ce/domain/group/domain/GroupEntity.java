3c8e10f594447b1c1d7aefa2008b95a0a374ffc4
YaroslavDrovolskyi <78113965+YaroslavDrovolskyi@users.noreply.github.com>
2025-09-13 23:35:38 +0300

    feat: impl migrations for group_students table

--- kub-backend-ce/src/main/java/education/kub/backend/ce/domain/group/domain/GroupEntity.java
+++ kub-backend-ce/src/main/java/education/kub/backend/ce/domain/group/domain/GroupEntity.java
@@ -1,10 +1,15 @@
 package education.kub.backend.ce.domain.group.domain;
 
+import education.kub.backend.ce.domain.academic_title.entity.AcademicTitleEntity;
+import education.kub.backend.ce.domain.role.entity.RoleEntity;
+import education.kub.backend.ce.domain.student.entity.StudentEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "groups")
@@ -24,4 +29,24 @@ public class GroupEntity {
 
     @Column(name = "created_at", columnDefinition = "timestamptz", nullable = false)
     private Instant createdAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_students",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private Set<StudentEntity> students = new HashSet<>();
+
+
+    public void addStudent(StudentEntity student) {
+        students.add(student);
+        student.getGroups().add(this);
+    }
+
+    public void removeRole(StudentEntity student) {
+        students.remove(student);
+        student.getGroups().remove(this);
+    }
+
 }
--- kub-backend-ce/src/main/java/education/kub/backend/ce/domain/student/entity/StudentEntity.java
+++ kub-backend-ce/src/main/java/education/kub/backend/ce/domain/student/entity/StudentEntity.java
@@ -1,11 +1,14 @@
 package education.kub.backend.ce.domain.student.entity;
 
+import education.kub.backend.ce.domain.group.domain.GroupEntity;
 import education.kub.backend.ce.domain.student_educational_program.domain.StudentEducationalProgramEntity;
 import education.kub.backend.ce.domain.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "students")
@@ -25,4 +28,7 @@ public class StudentEntity {
 
     @OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
     private List<StudentEducationalProgramEntity> studentEducationalPrograms;
+
+    @ManyToMany(mappedBy = "students")
+    private Set<GroupEntity> groups = new HashSet<>();
 }
