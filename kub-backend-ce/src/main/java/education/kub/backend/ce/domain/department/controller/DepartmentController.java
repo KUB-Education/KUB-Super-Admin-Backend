e93de4a4616b21fce3622485f584349fb91c0057
Tomash Oleksandr <Sa.tomash40@gmail.com>
2025-10-03 16:47:05 +0300

    fix: added api versin into Departments mapping

--- kub-backend-ce/src/main/java/education/kub/backend/ce/domain/department/controller/DepartmentController.java
+++ kub-backend-ce/src/main/java/education/kub/backend/ce/domain/department/controller/DepartmentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/departments")
+@RequestMapping("/api/v1/departments")
 @RequiredArgsConstructor
 @Validated
 public class DepartmentController {
