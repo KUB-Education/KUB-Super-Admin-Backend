af8ee41300cd7face4b8284c037d45c294215327
Олексій Нєвєдров <allex.nevedrov@gmail.com>
2025-10-13 19:54:27 +0300

    refactor: - change AccountProvider method signatures

--- kub-backend-ce/src/test/java/education/kub/backend/ce/domain/user/controller/AccountProvider.java
+++ kub-backend-ce/src/test/java/education/kub/backend/ce/domain/user/controller/AccountProvider.java
@@ -6,27 +6,27 @@ import static education.kub.backend.ce.domain.auth.controller.AuthProvider.Beare
 
 import io.restassured.http.Method;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
 
-public class AccountProvider extends RequestExecutor {
+public class AccountProvider  {
 
-    public static String GetUserAccountInfo(MockMvc mockMvc, String backend_url, String accessToken, HttpStatusCode expectedStatusCode) {
-        return ExecuteRequest(mockMvc, Method.GET, backend_url + "/api/v1/account/me", null,
+    public static String GetUserAccountInfo(RequestExecutor executor, String backend_url,
+                                            String accessToken, HttpStatusCode expectedStatusCode) {
+        return executor.ExecuteRequest(Method.GET, backend_url + "/api/v1/account/me", null,
                 null, null, BearerTokenToMap(accessToken),
                         expectedStatusCode, "schemas/UserDetailsResponse.json");
     }
 
-    public static String ChangePassword(MockMvc mockMvc, String backend_url, Map<String,String> request_body, String accessToken,
+    public static void ChangePassword(RequestExecutor executor, String backend_url, Map<String,String> request_body, String accessToken,
                                HttpStatusCode expectedStatusCode) {
-        return CommonRequest(mockMvc, backend_url + "/api/v1/account/change-password", request_body,
+        executor.CommonRequest(backend_url + "/api/v1/account/change-password", request_body,
                 BearerTokenToMap(accessToken), expectedStatusCode, "schemas/UserChangePasswordResponse.json");
     }
 
-    public static String RecoverPassword(MockMvc mockMvc, String backend_url, Map<String,String> request_body, String accessToken,
+    public static void RecoverPassword(RequestExecutor executor, String backend_url, Map<String,String> request_body, String accessToken,
                                 HttpStatusCode expectedStatusCode) {
-        return CommonRequest(mockMvc, backend_url + "/api/v1/account/recovery-password",
+        executor.CommonRequest(backend_url + "/api/v1/account/recovery-password",
                 request_body, BearerTokenToMap(accessToken), expectedStatusCode, null);
     }
 }
