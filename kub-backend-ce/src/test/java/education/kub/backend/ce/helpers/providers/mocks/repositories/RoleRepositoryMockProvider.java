290f253a4891dc04cec35281709fc4eaa83288ca
Олексій Нєвєдров <allex.nevedrov@gmail.com>
2025-10-27 23:16:21 +0200

    feat: add user and role entities repositories mock provider classes

---
+++ kub-backend-ce/src/test/java/education/kub/backend/ce/helpers/providers/mocks/repositories/RoleRepositoryMockProvider.java
@@ -0,0 +1,15 @@
+package education.kub.backend.ce.helpers.providers.mocks.repositories;
+
+import education.kub.backend.ce.domain.role.entity.RoleEntity;
+import education.kub.backend.ce.domain.role.repository.RoleRepository;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+public class RoleRepositoryMockProvider {
+    public static RoleRepository createRoleRepositoryMock(RoleEntity role) {
+        RoleRepository roleRepo = Mockito.mock(RoleRepository.class);
+        Mockito.lenient().when(roleRepo.findByType(role.getType())).thenReturn(Optional.of(role));
+        return roleRepo;
+    }
+}
