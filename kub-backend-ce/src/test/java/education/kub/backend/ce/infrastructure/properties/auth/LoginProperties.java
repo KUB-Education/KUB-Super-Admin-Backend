f58de6a871630b72ccf6e11c1211628cef2e91b3
Олексій Нєвєдров <allex.nevedrov@gmail.com>
2025-10-29 01:59:14 +0200

    chore: clean unused imports

--- kub-backend-ce/src/test/java/education/kub/backend/ce/infrastructure/properties/auth/LoginProperties.java
+++ kub-backend-ce/src/test/java/education/kub/backend/ce/infrastructure/properties/auth/LoginProperties.java
@@ -3,8 +3,6 @@ package education.kub.backend.ce.infrastructure.properties.auth;
 import education.kub.backend.ce.infrastructure.properties.executor.ExecutorProperties;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
