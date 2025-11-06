b0bb38c2979350ee56169491fa0ffa769e87ffbf
Олексій Нєвєдров <allex.nevedrov@gmail.com>
2025-11-06 13:13:52 +0200

    feat: improve UserController class functionality to support handling multiple user and role entities

--- kub-backend-ce/src/test/java/education/kub/backend/ce/infrastructure/components/user/UserComponent.java
+++ kub-backend-ce/src/test/java/education/kub/backend/ce/infrastructure/components/user/UserComponent.java
@@ -5,14 +5,12 @@ import education.kub.backend.ce.domain.role.repository.RoleRepository;
 import education.kub.backend.ce.domain.user.entity.UserEntity;
 import education.kub.backend.ce.domain.user.repository.UserRepository;
 
-import education.kub.backend.ce.infrastructure.properties.auth.LoginProperties;
 import education.kub.backend.ce.infrastructure.properties.user.UserProperties;
 import education.kub.backend.ce.infrastructure.providers.mocks.repositories.RoleRepositoryMockProvider;
 import education.kub.backend.ce.infrastructure.providers.mocks.services.TokenStoreServiceMockProvider;
 import education.kub.backend.ce.infrastructure.providers.mocks.repositories.UserRepositoryMockProvider;
 
 import education.kub.backend.ce.infrastructure.password.service.PasswordService;
-import education.kub.backend.ce.infrastructure.token.provider.JwtTokenProvider;
 import education.kub.backend.ce.infrastructure.token.store.service.TokenStoreService;
 
 import static education.kub.backend.ce.domain.user.entity.UserEntity.Status.ACTIVATED;
@@ -29,10 +27,7 @@ import java.util.HashSet;
 @Getter
 @Component
 public class UserComponent {
-    public LoginProperties loginData;
-    @Autowired
     public UserProperties userData;
-
     @Autowired
     public TokenStoreService tokenStoreService;
     @Autowired
@@ -41,53 +36,105 @@ public class UserComponent {
     public RoleRepository roleRepo;
     @Autowired
     public PasswordService passwordService;
-    @Autowired
-    public JwtTokenProvider jwtTokenProvider;
 
-    public void initRepos() {
-        roleRepo.deleteAll();
-        userRepo.deleteAll();
-        UserEntity user = createUser();
-        RoleEntity role = createRole(user);
-        injectRole(user, role);
-        roleRepo.save(role);
+    public String GetRoleIdString(RoleEntity.Type role) {
+        return Long.toString(roleRepo.findByType(role).get().getId());
+    }
+
+    private String GetFirstNotDublicatedRoleIdString(UserProperties properties) {
+        var user = userRepo.findById(properties.id).get();
+        for (long role_id = 0; role_id < RoleEntity.Type.values().length; role_id++) {
+            var role = roleRepo.findById(role_id);
+            if (role.isPresent() && role.get().getUsers().contains(user)) {
+                return Long.toString(role_id);
+            }
+        }
+        return null;
+    }
+
+    public String GetFirstNotDublicatedRoleIdString() {
+        return GetFirstNotDublicatedRoleIdString(userData);
+    }
+
+    private void addRole(long user_id, RoleEntity.Type roleType) {
+        var user = userRepo.findById(user_id).get();
+        var saved_role = roleRepo.findByType(roleType);
+        var role = saved_role.orElseGet(() -> createRole(user, roleType));
+        role.getUsers().add(user);
+        role = roleRepo.save(role);
+        user.addRole(role);
         userRepo.save(user);
     }
 
-    public void mockRepos() {
-        UserEntity user = createUser();
-        RoleEntity role = createRole(user);
-        user.setId(userData.id);
-        role.setId(userData.roles.id);
-        injectRole(user, role);
-        tokenStoreService = TokenStoreServiceMockProvider.createTokenStoreServiceMock();
-        userRepo = UserRepositoryMockProvider.createUserRepositoryMock(user);
-        roleRepo = RoleRepositoryMockProvider.createRoleRepositoryMock(role);
+    public void addRole(RoleEntity.Type roleType) {
+        addRole(userData.getId(), roleType);
     }
 
-    private RoleEntity createRole(UserEntity user) {
+    public UserEntity initRepos() {
+        roleRepo.deleteAll();
+        userRepo.deleteAll();
+        UserEntity user = createUser(userData);
+
+        for (var role: RoleEntity.Type.values()) {
+            if (userData.roles.contains(role)) {
+                RoleEntity roleEntity = createRole(user, role);
+                roleEntity = roleRepo.save(roleEntity);
+                user.addRole(roleEntity);
+            }
+            else {
+                RoleEntity roleEntity = createRole(role);
+                roleEntity = roleRepo.save(roleEntity);
+            }
+        }
+
+        userRepo.save(user);
+        return user;
+    }
+
+    public UserEntity mockRepos() {
+        UserEntity user = createUser(userData);
+        roleRepo = RoleRepositoryMockProvider.createRoleRepositoryMock();
+
+        for (var role: RoleEntity.Type.values()) {
+            if (userData.roles.contains(role)) {
+                RoleEntity roleEntity = createRole(user, role);
+                roleEntity = roleRepo.save(roleEntity);
+                user.addRole(roleEntity);
+            }
+            else {
+                RoleEntity roleEntity = createRole(role);
+                roleEntity = roleRepo.save(roleEntity);
+            }
+        }
+
+        tokenStoreService = TokenStoreServiceMockProvider.createTokenStoreServiceMock();
+        userRepo = UserRepositoryMockProvider.createUserRepositoryMock(user);
+
+        return user;
+    }
+
+    private RoleEntity createRole(RoleEntity.Type role) {
+        var roleEntity = new RoleEntity();
+        roleEntity.setType(role);
+        return roleEntity;
+    }
+
+    private RoleEntity createRole(UserEntity user, RoleEntity.Type role) {
         var userSet = new HashSet<UserEntity>();
         userSet.add(user);
-        var roleEntity = new RoleEntity();
-        roleEntity.setType(userData.roles.role);
+        var roleEntity = createRole(role);
         roleEntity.setUsers(userSet);
         return roleEntity;
     }
 
-    private UserEntity createUser() {
+    private UserEntity createUser(UserProperties userData) {
         UserEntity user = new UserEntity();
         user.setLastName(userData.last_name);
         user.setFirstName(userData.first_name);
         user.setMiddleName(userData.middle_name);
-        user.setEmail(loginData.email);
-        user.setPasswordHashed(passwordService.hash(loginData.password));
+        user.setEmail(userData.email);
+        user.setPasswordHashed(passwordService.hash(userData.password));
         user.setStatus(ACTIVATED);
         return user;
-    }
-
-    private void injectRole(UserEntity user, RoleEntity role) {
-        var roles = new HashSet<RoleEntity>();
-        roles.add(role);
-        user.setRoles(roles);
     }
 }
