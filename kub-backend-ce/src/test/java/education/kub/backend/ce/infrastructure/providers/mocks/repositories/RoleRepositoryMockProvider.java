b1ae9513a08ab82c03b3597c7b47c3031f7648c0
Олексій Нєвєдров <allex.nevedrov@gmail.com>
2025-11-06 13:13:03 +0200

    feat: improve user and role repository mock provider functionality to support handling multiple user and role entities

--- kub-backend-ce/src/test/java/education/kub/backend/ce/infrastructure/providers/mocks/repositories/RoleRepositoryMockProvider.java
+++ kub-backend-ce/src/test/java/education/kub/backend/ce/infrastructure/providers/mocks/repositories/RoleRepositoryMockProvider.java
@@ -2,14 +2,44 @@ package education.kub.backend.ce.infrastructure.providers.mocks.repositories;
 
 import education.kub.backend.ce.domain.role.entity.RoleEntity;
 import education.kub.backend.ce.domain.role.repository.RoleRepository;
+import education.kub.backend.ce.domain.user.entity.UserEntity;
 import org.mockito.Mockito;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+
 public class RoleRepositoryMockProvider {
-    public static RoleRepository createRoleRepositoryMock(RoleEntity role) {
+    private static long id_count = 0;
+
+    public static void resetMocks() {
+        id_count = 0;
+    }
+
+    private static void saveRole(RoleRepository mock, RoleEntity role) {
+        Mockito.lenient().doReturn(Optional.of(role)).when(mock).findById(role.getId());
+        Mockito.lenient().doReturn(Optional.of(role)).when(mock).findByType(role.getType());
+    }
+
+    public static RoleRepository createRoleRepositoryMock(UserEntity user) {
         RoleRepository roleRepo = Mockito.mock(RoleRepository.class);
-        Mockito.lenient().when(roleRepo.findByType(role.getType())).thenReturn(Optional.of(role));
+        Mockito.lenient().doAnswer(invocation -> {
+            var saved_role = invocation.getArgument(0, RoleEntity.class);
+            saved_role.setId(id_count++);
+            saveRole(roleRepo, saved_role);
+            return saved_role;
+        }).when(roleRepo).save(any(RoleEntity.class));
+        return roleRepo;
+    }
+
+    public static RoleRepository createRoleRepositoryMock() {
+        RoleRepository roleRepo = Mockito.mock(RoleRepository.class);
+        Mockito.lenient().doAnswer(invocation -> {
+            var saved_role = invocation.getArgument(0, RoleEntity.class);
+            saved_role.setId(id_count++);
+            saveRole(roleRepo, saved_role);
+            return saved_role;
+        }).when(roleRepo).save(any(RoleEntity.class));
         return roleRepo;
     }
 }
