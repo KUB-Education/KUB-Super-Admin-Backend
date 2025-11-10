a214da54d60aec3f7e6b5944a7a0c70cf71d49fa
Tomash Oleksandr <Sa.tomash40@gmail.com>
2025-11-10 12:23:03 +0200

    fix: replaced @Pattern by @NotBlank name in TimetableCreateRequest

--- kub-backend-ce/src/main/java/education/kub/backend/ce/domain/timetable/model/TimetableCreateRequest.java
+++ kub-backend-ce/src/main/java/education/kub/backend/ce/domain/timetable/model/TimetableCreateRequest.java
@@ -1,14 +1,13 @@
 package education.kub.backend.ce.domain.timetable.model;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.time.Instant;
 
 public record TimetableCreateRequest(
-        @Pattern(regexp = "^(?!\\s*$).+",
-                message = "Name must contain at least one non-whitespace character")
+        @NotBlank(message = "Timetable name can't be blank")
         @Size(min = 1, max = 64, message = "Timetable name must have length in interval [1,64]")
         String name,
 
